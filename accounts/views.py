ecbc153fb98be965d44ee4c892f47dc025d18d87
Joshua Xie <71893525+xiejoshua@users.noreply.github.com>
2026-09-10 22:23:47 -0400

    fix: auth_logout not found error

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
